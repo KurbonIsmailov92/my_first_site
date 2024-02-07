ca40b06656451ebd6dd0516800671d657d627090
KurbonIsmailov92 <kurbon.ismailov@gmail.com>
2024-02-07 17:56:56 +0500

    fixed bug, Create is working now

--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
+	_ "github.com/go-sql-driver/mysql"
 	"html/template"
 	"net/http"
 )
@@ -10,7 +12,7 @@ func HandleFunc() {
 	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("/static/css/main.css"))))
 	http.HandleFunc("/", Index)
 	http.HandleFunc("/create/", Create)
-	http.HandleFunc("/save_article/", SaveArticle)
+	http.HandleFunc("/save_article", SaveArticle)
 	http.ListenAndServe(":8080", nil)
 }
 
@@ -42,6 +44,21 @@ func SaveArticle(w http.ResponseWriter, r *http.Request) {
 	title := r.FormValue("title")
 	anons := r.FormValue("anons")
 	fullText := r.FormValue("full_text")
+
+	db, err := sql.Open("mysql", "root:@tcp(127.0.0.1:3306)/golang_db")
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	insert, err := db.Query(fmt.Sprintf("INSERT INTO articles (title, anons, full_text) VALUES('%s', '%s', '%s')", title, anons, fullText))
+	if err != nil {
+		panic(err)
+	}
+
+	defer insert.Close()
+
+	http.Redirect(w, r, "/", http.StatusSeeOther)
 
 }
 
